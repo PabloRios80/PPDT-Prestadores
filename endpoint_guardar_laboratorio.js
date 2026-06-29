c0a9b81383608879d56dfbc743a2123ae1c833a9
PabloRios80 <pablorios8080@gmail.com>
2026-06-29 11:43:47 -0300

    fixx

--- endpoint_guardar_laboratorio.js
+++ endpoint_guardar_laboratorio.js
@@ -123,6 +123,7 @@ function registrarEndpointGuardarLaboratorio(app, supabase) {
           .select("id")
           .eq("dni", dni)
           .ilike("descripcion_practica", `%${practica.descripcion}%`)
+          .not("descripcion_practica", "ilike", "%orina%")
           .eq("estado", "AUTORIZADA")
           .single();
 
